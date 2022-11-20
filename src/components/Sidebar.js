dfe44ac04e9a7c503358f954656235142133643e
Peter <peter.j.torki@gmail.com>
2022-11-21 00:03:33 +0200

    con

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,11 @@
 import React from "react"
 
 export default function Sidebar(props) {
+    {console.log("x")}
+                
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-                
                 className={`title ${
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
